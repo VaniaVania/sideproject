fb9982e865531e770fec4d8255fdb3d0a918c46c
VaniaVania <ivantumentsev@gmail.com>
2022-08-17 10:49:13 +0300

    Heroku Test 14

--- src/main/java/com/project/sideproject/SideProjectApplication.java
+++ src/main/java/com/project/sideproject/SideProjectApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SideProjectApplication{
     public static void main(String[] args) {
         SpringApplication.run(SideProjectApplication.class, args);
+
     }
 
 }
